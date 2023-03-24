5805e6d5c24b909ea53f55271029cdedabf83772
Drew Harris <harrisd@smu.edu>
2023-03-24 01:42:54 -0500

    Remove comment

--- examples/simple-react/vite.config.ts
+++ examples/simple-react/vite.config.ts
@@ -5,9 +5,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // optimizeDeps: {
-  //   exclude: ["flagsafe"],
-  // },
   resolve: {
     alias: {
       flagsafe: path.resolve(__dirname, "../../flagsafe/src/index.ts"),
